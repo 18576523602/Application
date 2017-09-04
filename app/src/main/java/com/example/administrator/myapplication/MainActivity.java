429a2b69be57ff47adc06caa78726dad95d08160
MS-20170513EKNR\Administrator <871547461@qq.com>
2017-09-04 16:33:14 +0800

    first send

---
+++ app/src/main/java/com/example/administrator/myapplication/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.administrator.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        TextView tv = (TextView) findViewById(R.id.sample_text);
+        tv.setText(stringFromJNI());
+        int a = 0;
+    }
+
+    public native String stringFromJNI();
+
+    static {
+        System.loadLibrary("native-lib");
+    }
+}
